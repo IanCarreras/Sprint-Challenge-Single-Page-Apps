6efc396594752f4d9246a6e5a41eafe09636ea65
Ian <mus5002@aol.com>
2019-09-14 18:01:10 -0400

    pagination working

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -4,12 +4,18 @@ import axios from 'axios'
 import CharacterCard from './CharacterCard'
 
 export default function CharacterList() {
+  const [info, setInfo] = useState()
   const [characterList, setCharacterList] = useState([])
+  const [change, setChange] = useState()
+  const [next, setNext] = useState()
+  const [previous, setPrevious] = useState()
 
   useEffect(() => {
     axios.get('https://rickandmortyapi.com/api/character/')
       .then(res => {
-        console.log(res.data)
+        setInfo(res.data.info)
+        setNext(res.data.info.next)
+        setPrevious(res.data.info.prev)
         return setCharacterList(res.data.results)
       })
       .catch(err => {
@@ -17,6 +23,23 @@ export default function CharacterList() {
       })
   }, []);
 
+  useEffect(() => {
+    axios.get(change)
+      .then(res => {
+        setInfo(res.data.info)
+        setNext(res.data.info.next)
+        setPrevious(res.data.info.prev)
+        return setCharacterList(res.data.results)
+      })
+      .catch(err => {
+        return err.response
+      })
+  }, [change])
+
+  const handleChange = (e) => {
+    return setChange(info[e.target.name])
+  }
+  
   return (
     <section>
       {
@@ -31,6 +54,12 @@ export default function CharacterList() {
                     origin={origin.name} />
         })
       }
+      <div>
+        {/* {previous && previous.includes('https') && <button name='previous' onClick={(e) => handleChange(e)}>Previous</button> }
+        {next && next.includes('https') && <button name='next' onClick={(e) => handleChange(e)}>Next</button> } */}
+        {<button disabled={!(previous && previous.includes('https'))} name='prev' onClick={(e) => handleChange(e)}>Previous</button>}
+        {<button disabled={!(next && next.includes('https'))} name='next' onClick={(e) => handleChange(e)}>Next</button>}
+      </div>
     </section>
   );
 }
